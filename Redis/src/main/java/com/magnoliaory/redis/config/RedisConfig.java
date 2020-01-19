3cb8baa8264976e7c3f70722b4a9b6b436bd3339
jorybeast <jorybeast97@foxmail.com>
2020-01-19 16:47:29 +0800

    redis

--- Redis/src/main/java/com/magnoliaory/redis/config/RedisConfig.java
+++ Redis/src/main/java/com/magnoliaory/redis/config/RedisConfig.java
@@ -1,34 +1,62 @@
 package com.magnoliaory.redis.config;
 
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.data.redis.support.collections.RedisProperties;
+
+import java.time.Duration;
 
 @Configuration
 @ConditionalOnClass(RedisOperations.class)
 @EnableConfigurationProperties(RedisProperties.class)
+@EnableCaching//开启缓存
 public class RedisConfig extends CachingConfigurerSupport {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String,Object> template = new RedisTemplate<>();
-
-        //配置连接工厂
-        template.setConnectionFactory(redisConnectionFactory);
-        //使用JDK的序列化方式
-        Jackson2JsonRedisSerializer jsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        //制定序列化的域
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.
+                defaultCacheConfig().entryTtl(Duration.ofSeconds(60));
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
+        RedisCacheManager manager = new RedisCacheManager(redisCacheWriter, cacheConfiguration);
+        return manager;
     }
+
+    @Bean
+    @SuppressWarnings("all")
+    public RedisTemplate<String, String> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        Jackson2JsonRedisSerializer jsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        jsonRedisSerializer.setObjectMapper(objectMapper);
+        RedisSerializer redisSerializer = new StringRedisSerializer();
+        stringRedisTemplate.setKeySerializer(redisSerializer);
+        stringRedisTemplate.setValueSerializer(jsonRedisSerializer);
+        stringRedisTemplate.setHashKeySerializer(redisSerializer);
+        stringRedisTemplate.setHashValueSerializer(jsonRedisSerializer);
+        stringRedisTemplate.afterPropertiesSet();
+        return stringRedisTemplate;
+    }
+    
+
 }
